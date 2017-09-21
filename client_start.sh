e0a47ab435e08144f6cc85fc478137ce10aafb5d
Justin Bell <justin.bell@mail.mcgill.ca>
2017-09-20 22:45:29 -0400

    First commit

---
+++ client_start.sh
@@ -0,0 +1,2 @@
+java -classpath `pwd`/bin \
+     -Djava.security.policy=java.policy client localhost 1099
---
+++ src/server/ResImpl/RMItem.java
@@ -0,0 +1,16 @@
+// -------------------------------
+// adapted from Kevin T. Manley
+// CSE 593
+// -------------------------------
+package server.ResImpl;
+
+import java.io.*;
+
+// Resource manager data item
+public abstract class RMItem implements Serializable
+{
+
+    RMItem() {
+			super();
+    }
+}
