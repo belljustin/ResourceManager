e3b0d46995cca31df6ae3c47e6a4d169696c1486
Justin Bell <justin.bell@mail.mcgill.ca>
2017-10-01 18:06:05 -0400

    Reformat to Google Java Style guide

--- src/server/tcp/FlightManagerTCP.java
+++ src/server/tcp/FlightManagerTCP.java
@@ -8,10 +8,8 @@ import java.rmi.RemoteException;
 import java.util.Vector;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
-
 import server.ResImpl.Flight;
 import server.ResImpl.Trace;
-
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -20,183 +18,176 @@ import java.net.ServerSocket;
 import java.net.Socket;
 
 public class FlightManagerTCP extends ResourceManagerTCP {
-	private ServerSocket serverSocket = null;
+  private ServerSocket serverSocket = null;
 
-	private ExecutorService executorService = Executors.newFixedThreadPool(10);
+  private ExecutorService executorService = Executors.newFixedThreadPool(10);
 
-	public static void main(String args[]) {
-		int port = 1099;
+  public static void main(String args[]) {
+    int port = 1099;
 
-		if (args.length == 1) {
-			port = Integer.parseInt(args[0]);
-		} else if (args.length > 1) {
-			System.err.println("Wrong usage");
-			System.out.println("Usage: java server.FlightManagerTCP [port]");
-			System.exit(1);
-		}
-
-		CarManagerTCP carRM = new CarManagerTCP(port);
-		carRM.runServer();
-	}
-
-	public FlightManagerTCP(int port) {
-		try {
-			serverSocket = new ServerSocket(port);
-		} catch (IOException e) {
-			System.err.printf("Could not listen on port: %d\n", port);
-			System.exit(1);
-		}
-	}
-
-	public void runServer() {
-		while (true) {
-			System.out.println("Waiting for request");
-			try {
-				Socket s = serverSocket.accept();
-				System.out.println("Processing request");
-				executorService.submit(new ServiceRequest(s));
-			} catch (IOException e) {
-				System.out.println("Error accepting connection");
-				e.printStackTrace();
-			}
-		}
-	}
-
-	class ServiceRequest implements Runnable {
-
-		private Socket socket;
-		PrintWriter out;
-		BufferedReader in;
-
-		public ServiceRequest(Socket connection) {
-			this.socket = connection;
-		}
-
-		public void run() {
-			int Id;
-			int flightNum;
-			int flightSeats;
-			int flightPrice;
-
-			String inputLine = null;
-
-			try {
-				out = new PrintWriter(socket.getOutputStream(), true);
-				in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-				while((inputLine = in.readLine()) != null) {
-					Vector<String> arguments = parse(inputLine);
-
-					String msg = "";
-					try {
-						switch (arguments.elementAt(0)) {
-						case "newflight":
-							Id = getInt(arguments.elementAt(1));
-				            flightNum = getInt(arguments.elementAt(2));
-				            flightSeats = getInt(arguments.elementAt(3));
-				            flightPrice = getInt(arguments.elementAt(4));
-				            msg = Boolean.toString(addFlight(Id,flightNum,flightSeats,flightPrice));
-							break;
-
-						case "deleteflight":
-							Id = getInt(arguments.elementAt(1));
-					        flightNum = getInt(arguments.elementAt(2));
-					        msg = Boolean.toString(deleteFlight(Id,flightNum));
-							break;
-
-						case "queryflight":
-							Id = getInt(arguments.elementAt(1));
-				            flightNum = getInt(arguments.elementAt(2));
-				            msg = Integer.toString(queryFlight(Id,flightNum));
-							break;
-
-						case "queryflightprice":
-							Id = getInt(arguments.elementAt(1));
-				            flightNum = getInt(arguments.elementAt(2));
-				            msg = Integer.toString(queryFlightPrice(Id,flightNum));
-							break;
-
-						case "reserveflight":
-							Id = getInt(arguments.elementAt(1));
-				            int customer = getInt(arguments.elementAt(2));
-				            flightNum = getInt(arguments.elementAt(3));
-				            msg = Boolean.toString(reserveFlight(Id,customer,flightNum));
-							break;
-
-						default:
-							msg = customerCases(arguments);
-						}
-					} catch (Exception e) {
-						// TODO: Handle this exception
-						e.printStackTrace();
-					}
-
-					out.println(msg);
-				}
-			} catch (IOException e) {
-				System.out.println("Exception caught when trying to read or write on socket");
-				System.out.println(e.getMessage());
-				return;
-			}
-
-			try {
-				socket.close();
-			} catch (IOException e) {
-				System.out.println("Error closing client connection");
-			}
-		}
-	}
-
-    // Create a new flight, or add seats to existing flight
-    //  NOTE: if flightPrice <= 0 and the flight already exists, it maintains its current price
-    public boolean addFlight(int id, int flightNum, int flightSeats, int flightPrice)
-        throws RemoteException
-    {
-        Trace.info("RM::addFlight(" + id + ", " + flightNum + ", $" + flightPrice + ", " + flightSeats + ") called" );
-        Flight curObj = (Flight) readData( id, Flight.getKey(flightNum) );
-        if ( curObj == null ) {
-            // doesn't exist...add it
-            Flight newObj = new Flight( flightNum, flightSeats, flightPrice );
-            writeData( id, newObj.getKey(), newObj );
-            Trace.info("RM::addFlight(" + id + ") created new flight " + flightNum + ", seats=" +
-                    flightSeats + ", price=$" + flightPrice );
-        } else {
-            // add seats to existing flight and update the price...
-            curObj.setCount( curObj.getCount() + flightSeats );
-            if ( flightPrice > 0 ) {
-                curObj.setPrice( flightPrice );
-            } // if
-            writeData( id, curObj.getKey(), curObj );
-            Trace.info("RM::addFlight(" + id + ") modified existing flight " + flightNum + ", seats=" + curObj.getCount() + ", price=$" + flightPrice );
-        } // else
-        return(true);
+    if (args.length == 1) {
+      port = Integer.parseInt(args[0]);
+    } else if (args.length > 1) {
+      System.err.println("Wrong usage");
+      System.out.println("Usage: java server.FlightManagerTCP [port]");
+      System.exit(1);
     }
 
+    CarManagerTCP carRM = new CarManagerTCP(port);
+    carRM.runServer();
+  }
 
-    
-    public boolean deleteFlight(int id, int flightNum)
-        throws RemoteException
-    {
-        return deleteItem(id, Flight.getKey(flightNum));
+  public FlightManagerTCP(int port) {
+    try {
+      serverSocket = new ServerSocket(port);
+    } catch (IOException e) {
+      System.err.printf("Could not listen on port: %d\n", port);
+      System.exit(1);
+    }
+  }
+
+  public void runServer() {
+    while (true) {
+      System.out.println("Waiting for request");
+      try {
+        Socket s = serverSocket.accept();
+        System.out.println("Processing request");
+        executorService.submit(new ServiceRequest(s));
+      } catch (IOException e) {
+        System.out.println("Error accepting connection");
+        e.printStackTrace();
+      }
+    }
+  }
+
+  class ServiceRequest implements Runnable {
+
+    private Socket socket;
+    PrintWriter out;
+    BufferedReader in;
+
+    public ServiceRequest(Socket connection) {
+      this.socket = connection;
     }
 
-    // Returns the number of empty seats on this flight
-    public int queryFlight(int id, int flightNum)
-        throws RemoteException
-    {
-        return queryNum(id, Flight.getKey(flightNum));
-    }
+    public void run() {
+      int Id;
+      int flightNum;
+      int flightSeats;
+      int flightPrice;
 
-    // Returns price of this flight
-    public int queryFlightPrice(int id, int flightNum )
-        throws RemoteException
-    {
-        return queryPrice(id, Flight.getKey(flightNum));
-    }
+      String inputLine = null;
 
-    // Adds flight reservation to this customer.  
-    public boolean reserveFlight(int id, int customerID, int flightNum)
-        throws RemoteException
-    {
-        return reserveItem(id, customerID, Flight.getKey(flightNum), String.valueOf(flightNum));
+      try {
+        out = new PrintWriter(socket.getOutputStream(), true);
+        in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+        while ((inputLine = in.readLine()) != null) {
+          Vector<String> arguments = parse(inputLine);
+
+          String msg = "";
+          try {
+            switch (arguments.elementAt(0)) {
+              case "newflight":
+                Id = getInt(arguments.elementAt(1));
+                flightNum = getInt(arguments.elementAt(2));
+                flightSeats = getInt(arguments.elementAt(3));
+                flightPrice = getInt(arguments.elementAt(4));
+                msg = Boolean.toString(addFlight(Id, flightNum, flightSeats, flightPrice));
+                break;
+
+              case "deleteflight":
+                Id = getInt(arguments.elementAt(1));
+                flightNum = getInt(arguments.elementAt(2));
+                msg = Boolean.toString(deleteFlight(Id, flightNum));
+                break;
+
+              case "queryflight":
+                Id = getInt(arguments.elementAt(1));
+                flightNum = getInt(arguments.elementAt(2));
+                msg = Integer.toString(queryFlight(Id, flightNum));
+                break;
+
+              case "queryflightprice":
+                Id = getInt(arguments.elementAt(1));
+                flightNum = getInt(arguments.elementAt(2));
+                msg = Integer.toString(queryFlightPrice(Id, flightNum));
+                break;
+
+              case "reserveflight":
+                Id = getInt(arguments.elementAt(1));
+                int customer = getInt(arguments.elementAt(2));
+                flightNum = getInt(arguments.elementAt(3));
+                msg = Boolean.toString(reserveFlight(Id, customer, flightNum));
+                break;
+
+              default:
+                msg = customerCases(arguments);
+            }
+          } catch (Exception e) {
+            // TODO: Handle this exception
+            e.printStackTrace();
+          }
+
+          out.println(msg);
+        }
+      } catch (IOException e) {
+        System.out.println("Exception caught when trying to read or write on socket");
+        System.out.println(e.getMessage());
+        return;
+      }
+
+      try {
+        socket.close();
+      } catch (IOException e) {
+        System.out.println("Error closing client connection");
+      }
     }
+  }
+
+  // Create a new flight, or add seats to existing flight
+  // NOTE: if flightPrice <= 0 and the flight already exists, it maintains its current price
+  public boolean addFlight(int id, int flightNum, int flightSeats, int flightPrice)
+      throws RemoteException {
+    Trace.info("RM::addFlight(" + id + ", " + flightNum + ", $" + flightPrice + ", " + flightSeats
+        + ") called");
+    Flight curObj = (Flight) readData(id, Flight.getKey(flightNum));
+    if (curObj == null) {
+      // doesn't exist...add it
+      Flight newObj = new Flight(flightNum, flightSeats, flightPrice);
+      writeData(id, newObj.getKey(), newObj);
+      Trace.info("RM::addFlight(" + id + ") created new flight " + flightNum + ", seats="
+          + flightSeats + ", price=$" + flightPrice);
+    } else {
+      // add seats to existing flight and update the price...
+      curObj.setCount(curObj.getCount() + flightSeats);
+      if (flightPrice > 0) {
+        curObj.setPrice(flightPrice);
+      } // if
+      writeData(id, curObj.getKey(), curObj);
+      Trace.info("RM::addFlight(" + id + ") modified existing flight " + flightNum + ", seats="
+          + curObj.getCount() + ", price=$" + flightPrice);
+    } // else
+    return (true);
+  }
+
+
+
+  public boolean deleteFlight(int id, int flightNum) throws RemoteException {
+    return deleteItem(id, Flight.getKey(flightNum));
+  }
+
+  // Returns the number of empty seats on this flight
+  public int queryFlight(int id, int flightNum) throws RemoteException {
+    return queryNum(id, Flight.getKey(flightNum));
+  }
+
+  // Returns price of this flight
+  public int queryFlightPrice(int id, int flightNum) throws RemoteException {
+    return queryPrice(id, Flight.getKey(flightNum));
+  }
+
+  // Adds flight reservation to this customer.
+  public boolean reserveFlight(int id, int customerID, int flightNum) throws RemoteException {
+    return reserveItem(id, customerID, Flight.getKey(flightNum), String.valueOf(flightNum));
+  }
 }
--- src/server/tcp/HotelManagerTCP.java
+++ src/server/tcp/HotelManagerTCP.java
@@ -7,10 +7,8 @@ package server.tcp;
 import java.util.Vector;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
-
 import server.ResImpl.Hotel;
 import server.ResImpl.Trace;
-
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -19,171 +17,173 @@ import java.net.ServerSocket;
 import java.net.Socket;
 
 public class HotelManagerTCP extends ResourceManagerTCP {
-	private ServerSocket serverSocket = null;
+  private ServerSocket serverSocket = null;
 
-	private ExecutorService executorService = Executors.newFixedThreadPool(10);
+  private ExecutorService executorService = Executors.newFixedThreadPool(10);
 
-	public static void main(String args[]) {
-		int port = 1099;
+  public static void main(String args[]) {
+    int port = 1099;
 
-		if (args.length == 1) {
-			port = Integer.parseInt(args[0]);
-		} else if (args.length > 1) {
-			System.err.println("Wrong usage");
-			System.out.println("Usage: java server.HotelManagerTCP [port]");
-			System.exit(1);
-		}
-
-		CarManagerTCP carRM = new CarManagerTCP(port);
-		carRM.runServer();
-	}
-
-	public HotelManagerTCP(int port) {
-		try {
-			serverSocket = new ServerSocket(port);
-		} catch (IOException e) {
-			System.err.printf("Could not listen on port: %d\n", port);
-			System.exit(1);
-		}
-	}
-
-	public void runServer() {
-		while (true) {
-			System.out.println("Waiting for request");
-			try {
-				Socket s = serverSocket.accept();
-				System.out.println("Processing request");
-				executorService.submit(new ServiceRequest(s));
-			} catch (IOException e) {
-				System.out.println("Error accepting connection");
-				e.printStackTrace();
-			}
-		}
-	}
-
-	class ServiceRequest implements Runnable {
-		private Socket socket;
-		PrintWriter out;
-		BufferedReader in;
-
-		public ServiceRequest(Socket connection) {
-			this.socket = connection;
-		}
-
-		public void run() {
-			int Id;
-			String location;
-			int numRooms;
-			int price;
-			String inputLine = null;
-
-			try {
-				out = new PrintWriter(socket.getOutputStream(), true);
-				in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-				while((inputLine = in.readLine()) != null) {
-					Vector<String> arguments = parse(inputLine);
-
-					String msg = "";
-					try {
-						switch (arguments.elementAt(0)) {
-						case "newroom":
-							Id = getInt(arguments.elementAt(1));
-				            location = getString(arguments.elementAt(2));
-				            numRooms = getInt(arguments.elementAt(3));
-				            price = getInt(arguments.elementAt(4));
-				            msg = Boolean.toString(addRooms(Id,location,numRooms,price));
-							break;
-
-						case "deleteroom":
-							Id = getInt(arguments.elementAt(1));
-							location = getString(arguments.elementAt(2));
-							msg = Boolean.toString(deleteRooms(Id,location));
-							break;
-
-						case "queryroom":
-							Id = getInt(arguments.elementAt(1));
-				            location = getString(arguments.elementAt(2));
-				            msg = Integer.toString(queryRooms(Id,location));
-							break;
-
-						case "queryroomprice":
-							Id = getInt(arguments.elementAt(1));
-				            location = getString(arguments.elementAt(2));
-				            msg = Integer.toString(queryRoomsPrice(Id,location));
-							break;
-
-						case "reserveroom":
-							Id = getInt(arguments.elementAt(1));
-				            int customer = getInt(arguments.elementAt(2));
-				            location = getString(arguments.elementAt(3));
-				            
-				            msg = Boolean.toString(reserveRoom(Id,customer,location));
-							break;
-
-						default:
-							msg = customerCases(arguments);
-						}
-					} catch (Exception e) {
-						// TODO: Handle this exception
-						e.printStackTrace();
-					}
-
-					out.println(msg);
-				}
-			} catch (IOException e) {
-				System.out.println("Exception caught when trying to read or write on socket");
-				System.out.println(e.getMessage());
-				return;
-			}
-
-			try {
-				socket.close();
-			} catch (IOException e) {
-				System.out.println("Error closing client connection");
-			}
-		}
-	}
-
-    // Create a new room location or add rooms to an existing location
-    //  NOTE: if price <= 0 and the room location already exists, it maintains its current price
-    public boolean addRooms(int id, String location, int count, int price) {
-        Trace.info("RM::addRooms(" + id + ", " + location + ", " + count + ", $" + price + ") called" );
-        Hotel curObj = (Hotel) readData( id, Hotel.getKey(location) );
-        if ( curObj == null ) {
-            // doesn't exist...add it
-            Hotel newObj = new Hotel( location, count, price );
-            writeData( id, newObj.getKey(), newObj );
-            Trace.info("RM::addRooms(" + id + ") created new room location " + location + ", count=" + count + ", price=$" + price );
-        } else {
-            // add count to existing object and update price...
-            curObj.setCount( curObj.getCount() + count );
-            if ( price > 0 ) {
-                curObj.setPrice( price );
-            } // if
-            writeData( id, curObj.getKey(), curObj );
-            Trace.info("RM::addRooms(" + id + ") modified existing location " + location + ", count=" + curObj.getCount() + ", price=$" + price );
-        } // else
-        return(true);
+    if (args.length == 1) {
+      port = Integer.parseInt(args[0]);
+    } else if (args.length > 1) {
+      System.err.println("Wrong usage");
+      System.out.println("Usage: java server.HotelManagerTCP [port]");
+      System.exit(1);
     }
 
-    // Delete rooms from a location
-    public boolean deleteRooms(int id, String location) {
-        return deleteItem(id, Hotel.getKey(location));
-        
+    CarManagerTCP carRM = new CarManagerTCP(port);
+    carRM.runServer();
+  }
+
+  public HotelManagerTCP(int port) {
+    try {
+      serverSocket = new ServerSocket(port);
+    } catch (IOException e) {
+      System.err.printf("Could not listen on port: %d\n", port);
+      System.exit(1);
+    }
+  }
+
+  public void runServer() {
+    while (true) {
+      System.out.println("Waiting for request");
+      try {
+        Socket s = serverSocket.accept();
+        System.out.println("Processing request");
+        executorService.submit(new ServiceRequest(s));
+      } catch (IOException e) {
+        System.out.println("Error accepting connection");
+        e.printStackTrace();
+      }
+    }
+  }
+
+  class ServiceRequest implements Runnable {
+    private Socket socket;
+    PrintWriter out;
+    BufferedReader in;
+
+    public ServiceRequest(Socket connection) {
+      this.socket = connection;
     }
 
-    // Returns the number of rooms available at a location
-    public int queryRooms(int id, String location) {
-        return queryNum(id, Hotel.getKey(location));
-    }
+    public void run() {
+      int Id;
+      String location;
+      int numRooms;
+      int price;
+      String inputLine = null;
 
-    // Returns room price at this location
-    public int queryRoomsPrice(int id, String location) {
-        return queryPrice(id, Hotel.getKey(location));
-    }
+      try {
+        out = new PrintWriter(socket.getOutputStream(), true);
+        in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+        while ((inputLine = in.readLine()) != null) {
+          Vector<String> arguments = parse(inputLine);
 
-    // Adds room reservation to this customer. 
-    public boolean reserveRoom(int id, int customerID, String location) {
-        return reserveItem(id, customerID, Hotel.getKey(location), location);
+          String msg = "";
+          try {
+            switch (arguments.elementAt(0)) {
+              case "newroom":
+                Id = getInt(arguments.elementAt(1));
+                location = getString(arguments.elementAt(2));
+                numRooms = getInt(arguments.elementAt(3));
+                price = getInt(arguments.elementAt(4));
+                msg = Boolean.toString(addRooms(Id, location, numRooms, price));
+                break;
+
+              case "deleteroom":
+                Id = getInt(arguments.elementAt(1));
+                location = getString(arguments.elementAt(2));
+                msg = Boolean.toString(deleteRooms(Id, location));
+                break;
+
+              case "queryroom":
+                Id = getInt(arguments.elementAt(1));
+                location = getString(arguments.elementAt(2));
+                msg = Integer.toString(queryRooms(Id, location));
+                break;
+
+              case "queryroomprice":
+                Id = getInt(arguments.elementAt(1));
+                location = getString(arguments.elementAt(2));
+                msg = Integer.toString(queryRoomsPrice(Id, location));
+                break;
+
+              case "reserveroom":
+                Id = getInt(arguments.elementAt(1));
+                int customer = getInt(arguments.elementAt(2));
+                location = getString(arguments.elementAt(3));
+
+                msg = Boolean.toString(reserveRoom(Id, customer, location));
+                break;
+
+              default:
+                msg = customerCases(arguments);
+            }
+          } catch (Exception e) {
+            // TODO: Handle this exception
+            e.printStackTrace();
+          }
+
+          out.println(msg);
+        }
+      } catch (IOException e) {
+        System.out.println("Exception caught when trying to read or write on socket");
+        System.out.println(e.getMessage());
+        return;
+      }
+
+      try {
+        socket.close();
+      } catch (IOException e) {
+        System.out.println("Error closing client connection");
+      }
     }
+  }
+
+  // Create a new room location or add rooms to an existing location
+  // NOTE: if price <= 0 and the room location already exists, it maintains its current price
+  public boolean addRooms(int id, String location, int count, int price) {
+    Trace.info("RM::addRooms(" + id + ", " + location + ", " + count + ", $" + price + ") called");
+    Hotel curObj = (Hotel) readData(id, Hotel.getKey(location));
+    if (curObj == null) {
+      // doesn't exist...add it
+      Hotel newObj = new Hotel(location, count, price);
+      writeData(id, newObj.getKey(), newObj);
+      Trace.info("RM::addRooms(" + id + ") created new room location " + location + ", count="
+          + count + ", price=$" + price);
+    } else {
+      // add count to existing object and update price...
+      curObj.setCount(curObj.getCount() + count);
+      if (price > 0) {
+        curObj.setPrice(price);
+      } // if
+      writeData(id, curObj.getKey(), curObj);
+      Trace.info("RM::addRooms(" + id + ") modified existing location " + location + ", count="
+          + curObj.getCount() + ", price=$" + price);
+    } // else
+    return (true);
+  }
+
+  // Delete rooms from a location
+  public boolean deleteRooms(int id, String location) {
+    return deleteItem(id, Hotel.getKey(location));
+
+  }
+
+  // Returns the number of rooms available at a location
+  public int queryRooms(int id, String location) {
+    return queryNum(id, Hotel.getKey(location));
+  }
+
+  // Returns room price at this location
+  public int queryRoomsPrice(int id, String location) {
+    return queryPrice(id, Hotel.getKey(location));
+  }
+
+  // Adds room reservation to this customer.
+  public boolean reserveRoom(int id, int customerID, String location) {
+    return reserveItem(id, customerID, Hotel.getKey(location), location);
+  }
 }
