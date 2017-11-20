1f0105d1164ec7dd4124b634d985e5dc0d0c098a
Justin Bell <justin.bell@mail.mcgill.ca>
2017-11-20 18:15:38 -0500

    Fix server start script

--- scripts/server_start.sh
+++ scripts/server_start.sh
@@ -1,14 +1,14 @@
 java -classpath "$PROJECTPATH/bin/" \
      -Djava.security.policy="$PROJECTPATH/java.policy" \
      -Djava.rmi.server.codebase="file:$PROJECTPATH/bin/" \
-     server.ResImpl.FlightManagerImpl 1099 &
+     server.ResImpl.FlightManager 1099 &
 
 java -classpath ../bin/ \
      -Djava.security.policy="$PROJECTPATH/java.policy" \
      -Djava.rmi.server.codebase="file:$PROJECTPATH/bin/" \
-     server.ResImpl.HotelManagerImpl 1099 &
+     server.ResImpl.HotelManager 1099 &
 
 java -classpath ../bin/ \
      -Djava.security.policy="$PROJECTPATH/java.policy" \
      -Djava.rmi.server.codebase="file:$PROJECTPATH/bin/" \
-     server.ResImpl.CarManagerImpl 1099 &
+     server.ResImpl.CarManager 1099 &
